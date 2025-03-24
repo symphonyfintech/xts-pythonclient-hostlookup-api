386a69e2d0f6083b921979eab46a63e2311398bf
anitapoola <49015957+anitapoola@users.noreply.github.com>
2025-03-24 11:07:28 +0530

    Update MarketDataSocketClient.py

--- MarketDataSocketClient.py
+++ MarketDataSocketClient.py
@@ -48,7 +48,7 @@ class MDSocket_io(socketio.Client):
         self.sid.on('connect', self.on_connect)
         self.sid.on('message', self.on_message)
 
-         """Similarly implement partial json full and binary json full."""
+        """Similarly implement partial json full and binary json full."""
         self.sid.on('1501-json-full', self.on_message1501_json_full)
         self.sid.on('1501-json-partial', self.on_message1501_json_partial)
 
@@ -58,15 +58,12 @@ class MDSocket_io(socketio.Client):
         self.sid.on('1505-json-full', self.on_message1505_json_full)
         self.sid.on('1505-json-partial', self.on_message1505_json_partial)
 
-        self.sid.on('1507-json-full', self.on_message1507_json_full)
-
         self.sid.on('1510-json-full', self.on_message1510_json_full)
         self.sid.on('1510-json-partial', self.on_message1510_json_partial)
 
         self.sid.on('1512-json-full', self.on_message1512_json_full)
         self.sid.on('1512-json-partial', self.on_message1512_json_partial)
 
-        self.sid.on('1105-json-full', self.on_message1105_json_full)
         self.sid.on('1105-json-partial', self.on_message1105_json_partial)
 
         self.sid.on('disconnect', self.on_disconnect)
@@ -134,13 +131,10 @@ class MDSocket_io(socketio.Client):
         """On receiving message code 1502 full"""
         print('I received a 1502 Market depth message!' + data)
 
-   def on_message1507_json_full(self, data):
-        """On receiving message code 1507 full"""
-        print('I received a 1507 MarketStatus message!' + data)
         
-   def on_message1512_json_full(self, data):
-        """On receiving message code 1512 full"""
-        print('I received a 1512 LTP message!' + data)     
+    def on_message1512_json_full(self, data):
+            """On receiving message code 1512 full"""
+            print('I received a 1512 LTP message!' + data)     
 
     def on_message1505_json_full(self, data):
         """On receiving message code 1505 full"""
